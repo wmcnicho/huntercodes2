520a5f1d240ee6cdccf4b38ecdcb19ef81661bf4
William McNichols <wmcnicho@usc.edu>
2021-12-15 17:40:01 -0500

    Alternative redirect style

--- src/components/About.js
+++ src/components/About.js
@@ -25,12 +25,12 @@ class About extends React.Component {
                             </Box>
                             <Typography variant="h4">Hunter McNichols</Typography>
                             <Typography variant="h5">Engineer and Educator</Typography>
-                            <Typography variant="h5"><Link href='https://www.ccny.cuny.edu/'>CCNY</Link> <Link href='https://huntercodes.com/hunter'>HunterCodes</Link></Typography>
+                            <Typography variant="h5"><Link href='https://www.ccny.cuny.edu/'>CCNY</Link> <Link href='#'>HunterCodes</Link></Typography>
                         </Box>
                     </Grid>
                     <Grid item md={8} xs={12} sx={{textAlign:'left'}}>
                         <Typography variant="h4">Who is Hunter?</Typography>
-                        <Typography>I have been obsessed with computers and technology for longer than my working memory. This vice led me to pursue a join bachelor-master degree in Computer Science at University of Southern California ✌️.<br/><br/></Typography>
+                        <Typography>I have been obsessed with computers and technology for longer than my working memory. This vice led me to pursue a joint bachelor-master degree in Computer Science at University of Southern California ✌️.<br/><br/></Typography>
                         <Typography>After graduating, I worked at Apple as a software engineer on Siri. I worked on a cross-platform team and developed server-side frameworks, mobile frameworks, and just about everything in between. A very scary stack of NDAs make this experience hard describe in detail, but I can say that I have become intimately familiar with GUS architectures and the challenges in building these systems at scale.<br/><br/></Typography>
                         <Typography>In 2019, I relocated from the Bay Area to New York where I continue to get lost in the subway systems and remind myself what seasons are. Here I started HunterCodes and have worked with small businesses and startups to bring their ideas online. I mostly do web related builds so you could call me a full-stack engineer but I work in offline environments as well.<br/><br/></Typography>
                         <Typography>One year later, I started teaching a course on teach Artificial Intelligence at City College of New York. I love to teach and doing so has reignited my passion for computer science. If you are a prospective student you can take a gander at the syllabus and assignments from a previous semester. I am now aspiring to go back into academics and get a PhD in Computer Science.<br/><br/></Typography>
